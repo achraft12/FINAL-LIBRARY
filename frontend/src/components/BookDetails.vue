a59edc4bc8121cef9de0ed0ce868f75cbad96a7d
achraft12 <achrafachraftaki@gmail.com>
2024-11-19 23:13:36 +0100

    Update BookDetails.vue

--- frontend/src/components/BookDetails.vue
+++ frontend/src/components/BookDetails.vue
@@ -36,7 +36,7 @@ export default {
   created() {
     const bookId = this.$route.params.id;
     axios
-      .get(`http://localhost:3000/books/${bookId}`)
+      .get(`https://final-librarybackend.onrender.com/books/${bookId}`)
       .then((response) => {
         this.book = response.data;
       })
